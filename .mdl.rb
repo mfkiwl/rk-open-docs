55ef6de67e9afd97fd9bfab8132035ecd1a992cc
Tao Huang <huangtao@rock-chips.com>
2018-12-25 10:22:00 +0800

    .mdl.rb: adjust MD009 parameters
    The br_spaces parameter allows an exception to this rule for a specific
    amount of trailing spaces used to insert an explicit line break/br
    element. For example, set br_spaces to 2 to allow exactly 2 spaces at
    the end of a line.
    
    Change-Id: I8015cb7697a95ab5a3836b82e3169e5e7d282fef
    Signed-off-by: Tao Huang <huangtao@rock-chips.com>

--- .mdl.rb
+++ .mdl.rb
@@ -3,7 +3,8 @@ all
 exclude_rule 'fenced-code-language' # Fenced code blocks should have a language specified
 exclude_rule 'first-line-h1' # First line in file should be a top level header
 
+exclude_rule 'MD010' # Hard tabs
 exclude_rule 'MD013' # Line length
 exclude_rule 'MD036' # Emphasis used instead of a header
-exclude_rule 'MD010' # Hard tabs
-rule 'MD029', :style => "ordered"
+rule 'MD009', :br_spaces => 2 # Trailing spaces
+rule 'MD029', :style => "ordered" # Ordered list item prefix
