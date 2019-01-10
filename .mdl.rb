3532e2e397fead687b4c9355a27fac0e26f4a098
Tao Huang <huangtao@rock-chips.com>
2019-01-10 19:58:00 +0800

    .mdl.rb: exclude rule MD033 Inline HTML
    Change-Id: I595c6945f99e05ff4338e2850a0e49caab702a20
    Signed-off-by: Tao Huang <huangtao@rock-chips.com>

--- .mdl.rb
+++ .mdl.rb
@@ -5,6 +5,7 @@ exclude_rule 'first-line-h1' # First line in file should be a top level header
 
 exclude_rule 'MD010' # Hard tabs
 exclude_rule 'MD013' # Line length
+exclude_rule 'MD033' # Inline HTML
 exclude_rule 'MD036' # Emphasis used instead of a header
 rule 'MD009', :br_spaces => 2 # Trailing spaces
 rule 'MD029', :style => "ordered" # Ordered list item prefix
