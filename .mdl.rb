168f6b4eaaf606f54f956e7e0df7371f55756b98
Tao Huang <huangtao@rock-chips.com>
2020-07-13 18:08:14 +0800

    .mdl.rb: adjust MD024 parameters
    This rule is triggered if there are multiple headers in the document
    that have the same text.
    
    If the parameter allow_different_nesting is set to true, header
    duplication under different nesting is allowed.
    
    Change-Id: Ib376d476da550be56bf1b353211eab0d35f29b2d
    Signed-off-by: Tao Huang <huangtao@rock-chips.com>

--- .mdl.rb
+++ .mdl.rb
@@ -9,4 +9,5 @@ exclude_rule 'MD033' # Inline HTML
 exclude_rule 'MD036' # Emphasis used instead of a header
 rule 'MD007', :indent => 4 # Unordered list indentation
 rule 'MD009', :br_spaces => 2 # Trailing spaces
+rule 'MD024', :allow_different_nesting => true # header duplication under different nesting is allowed
 rule 'MD029', :style => "ordered" # Ordered list item prefix
